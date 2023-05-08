460a7b99ac866531a9d8cb85290080e2d713d37e
Victor Candido <victorjrcandido@gmail.com>
2023-05-08 16:02:18 -0300

    small fixes

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,17 +2,18 @@ import React from 'react'
 import "./Hero.css"
 import { IoMenuSharp } from "react-icons/io5";
 import { IconContext } from "react-icons";
+const burgerIcon = <IconContext.Provider value={{ size: "2em", className: "menu-icon", color: "black" }}>
+  <div>
+    <IoMenuSharp />
+  </div>
+</IconContext.Provider>
 
 const Hero = () => {
   return (
     <div className='hero-container'>
       <div className='nav'>
         <img className='nav-logo' src='/logo.png' alt='logo' />
-        <IconContext.Provider value={{ size: "2em", className: "menu-icon", color: "black" }}>
-          <div>
-            <IoMenuSharp />
-          </div>
-        </IconContext.Provider>
+        <button className='btn'>{burgerIcon}</button>
       </div>
       <div className='hero-section'>
         <img className='hero-img' src='/hero-image.png' alt='banner' />
